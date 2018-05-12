ed66efe8530593891a2e80a097b503ea4ca7c5a7
Alexander Granin <graninas@gmail.com>
2018-05-13 00:46:14 +0700

    TODOs removed.

--- src/Control/Concurrent/STM/Free/Internal/Types.hs
+++ src/Control/Concurrent/STM/Free/Internal/Types.hs
@@ -5,23 +5,6 @@ module Control.Concurrent.STM.Free.Internal.Types where
 import           Control.Concurrent.STM.Free.Internal.Imports
 import qualified Data.HMap                                    as HMap
 
--- TODOs:
--- + Remove FromJSON / ToJSON limitation
--- + Conflicts resolving: remove string building as conflict indicator
--- + Clone on write
--- + Remove IORef from TVarHandle.
--- - Pregenerate a lot of uniques.
--- - Replace Data.Unique by own ids (on base of Int).
--- - Store context id in every TVar. This will allow to detect TVars in wrong contexts.
--- - Replace MVar lock with atomicModifyIORef'
--- - Replace Free with Freer ("No Remorse")
--- - Make runSTM tail-recursive
--- - Remove runStateT, make it IORef
--- - Remove time backoff, make it eventual
-
--- A64m qb0:
--- тогда поискать возможность чтоб ключи были Int или несколько интов, использовать хештаблицу из ghc-ного рантайма с индексами в мутабельном массиве Any, с кастами к нужному типу быстрее, думаю, не сделать
-
 type UStamp = Unique
 type OrigUStamp = UStamp
 type UpdatedUStamp = UStamp
