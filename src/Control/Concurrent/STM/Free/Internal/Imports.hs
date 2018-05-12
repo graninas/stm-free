3a1875b06d8ee7a41ec0c4b7fa11f361b827518f
Alexander Granin <graninas@gmail.com>
2018-05-13 00:27:39 +0700

    Imports added.

---
+++ src/Control/Concurrent/STM/Free/Internal/Imports.hs
@@ -0,0 +1,18 @@
+module Control.Concurrent.STM.Free.Internal.Imports
+  ( module X
+  , module Map
+  ) where
+
+import           Control.Concurrent.MVar    as X (MVar, newEmptyMVar, newMVar,
+                                                  putMVar, takeMVar)
+import           Control.Monad              as X (unless, void, when)
+import           Control.Monad.Free         as X (Free (..), liftF)
+import           Control.Monad.IO.Class     as X (liftIO)
+import           Control.Monad.State.Strict as X (StateT, evalStateT,
+                                                  execStateT, get, gets, modify,
+                                                  put, runStateT)
+import           Data.IORef                 as X (IORef, modifyIORef', newIORef,
+                                                  readIORef, writeIORef)
+import qualified Data.Map                   as Map
+import           Data.Unique                as X (Unique, hashUnique, newUnique)
+
